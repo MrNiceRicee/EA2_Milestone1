91d4fc058a21655e8ec8c46327cc793686cb692a
MrNiceRicee <47019696+MrNiceRicee@users.noreply.github.com>
2020-05-14 15:02:36 -0700

    initiate

---
+++ Minesweeper/Minesweeper/Board.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    class Board
+    {
+        public int Size { get; set; }
+
+        public Cell[,] Grid { get; set; }
+
+        public double Difficulty { get; set; }
+
+        Random random = new Random();
+
+        public Board(int size)
+        {
+            Size = size;
+            Grid = new Cell[size, size];
+            Difficulty = .5;
+            setupLiveNeighbors(Grid);
+        }
+
+
+        public void setupLiveNeighbors(Cell[,] cells)
+        {
+            int LiveNumbers = 0;
+            int count = 0;
+            for (int col = 0; col < cells.GetLength(0); col++)
+            {
+                for (int row = 0; row < cells.GetLength(1); row++)
+                {
+                    cells[col, row] = new Cell();
+                    count++;
+                    if (LiveNumbers < ((Size*Size)*Difficulty)) {
+                        //hard limit the amount of cells to be live to the difficulty
+                        if (random.Next(0, (Size*Size)) > ((Size*Size)*Difficulty))
+                        {
+                            //get total max and multiplied by difficulty, to find the point to calculate the percentage
+                            cells[col, row].Live = true;
+                            LiveNumbers++;
+                            Console.WriteLine("Position:"+count+". Live:"+LiveNumbers+".\t"+col +","+row);
+                        }
+                    }
+                }
+                
+            }
+        }
+
+    }
+}
---
+++ Minesweeper/Minesweeper/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Board x = new Board(10);
+            Console.WriteLine(printBoard(x));
+            Console.Read();
+        }
+
+        public static String printBoard(Board board)
+        {
+            String r = "";
+            for (int row = 0; row < board.Grid.GetLength(0); row++)
+            {
+                for (int i = 0; i < board.Grid.GetLength(1); i++)
+                {
+                         
+                    r += "----";
+                }
+                //do pretty lines on the board
+                r += "\n";
+                for (int col = 0; col < board.Grid.GetLength(1); col++)
+                {
+                    var cell = board.Grid[row, col];
+                    if (cell.Live)
+                    {
+                        r += "| o ";
+                    }else
+                    {
+                        r += "| x ";
+                    }
+                }
+                r += "|\n";
+
+                //started a new line
+            }
+            for (int i = 0; i < board.Grid.GetLength(1); i++)
+            {
+                r += "----";
+            }
+            // last separation line on the board
+            return r;
+        }
+    }
+}
